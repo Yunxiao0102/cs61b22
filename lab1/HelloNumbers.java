2493546a573e71a4019a681a5f81c5b919443e7e
Yunxiao0102 <108102842+Yunxiao0102@users.noreply.github.com>
2022-07-23 17:33:39 -0400

    Add files via upload

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int m = 0;
+        while (x < 10) {
+            m = m + x;
+            System.out.print(m + " ");
+            x = x + 1;
+        }
+    }
+}
